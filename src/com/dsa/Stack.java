a3025589d41e6724879dc486603827b33f020254
PackiyaMuni-G <dumuni007@gmail.com>
2024-11-08 19:24:39 +0530

    commit with Stack peek and pop

--- src/com/dsa/Stack.java
+++ src/com/dsa/Stack.java
@@ -3,17 +3,23 @@ package com.dsa;
 public class Stack {
 	
 	int[] arr=new int[5];
+	int size;
 	int top;
 	
 	public Stack() {
-		int size=arr.length;
+		 size=arr.length;
 		int top=-1;
 	}
 
 	public void push(int data) {
-	
-	  arr[top]=data;
-	  top++;
+	 if(top > -1) {
+		  arr[top]=data;
+		  top++;
+	 }
+	 else {
+		 System.out.println("Stack is empty");
+	 }
+
 	}
   public void printStack() {
 	  for(int n:arr) {
@@ -21,4 +27,14 @@ public class Stack {
 	  }
 	  System.out.println();
   }
+
+public int pop() {
+        //remove & return an element
+	return arr[top--];
+}
+
+public int peek() {
+     //only remove last element
+	return arr[top];
+}
 }
--- src/com/dsa/StackExample.java
+++ src/com/dsa/StackExample.java
@@ -8,12 +8,16 @@ public class StackExample {
 	Stack nums=new Stack();
 	nums.push(10);
 	nums.push(15);
+	System.out.println(nums.peek());
 	nums.push(20);
 	nums.push(25);
+	
 //	System.out.println(nums);
 //     System.out.println(nums.pop()); 
 
     nums.printStack();
+ 
+  
 	}
 
 }
